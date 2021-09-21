6bfa8c19c985a5fbf6b4899def368b568a17aaf9
Anderson Souza <andersonfebs@gmail.com>
2021-09-21 00:27:15 -0400

    update

--- modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -6,10 +6,12 @@ interface IRequest {
 }
 
 class CreateCategoryUseCase {
-  constructor(private categoriesRepository: ICategoriesRepository) {}
+  constructor(private categoriesRepository: ICategoriesRepository) { }
 
-  execute({ description, name }: IRequest): void {
-    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+  async execute({ description, name }: IRequest): Promise<void> {
+    const categoryAlreadyExists = await this.categoriesRepository.findByName(
+      name
+    );
 
     if (categoryAlreadyExists) {
       throw new Error("Category already exists!");
--- modules/cars/useCases/listSpecifications/ListSpecificationsUseCase.ts
+++ modules/cars/useCases/listSpecifications/ListSpecificationsUseCase.ts
@@ -1,8 +1,8 @@
-import { Specification } from "../../model/Specification";
+import { Specification } from "../../entities/Specification";
 import { SpecificationsRepository } from "../../repositories/implementations/SpecificationsRepository";
 
 class ListSpecificationsUseCase {
-  constructor(private specificationsRepository: SpecificationsRepository) {}
+  constructor(private specificationsRepository: SpecificationsRepository) { }
 
   execute(): Specification[] {
     const specifications = this.specificationsRepository.list();
